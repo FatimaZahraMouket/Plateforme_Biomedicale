aab9bc4e262ca0bf9b2554a2f578e9bd22c2f5c8
unknown <fatimazahra20mouket@gmail.com>
2024-01-21 22:51:37 +0100

    update arduino code

--- partie iot/sketch_nov29a/sketch_nov29a.ino
+++ partie iot/sketch_nov29a/sketch_nov29a.ino
@@ -6,35 +6,33 @@ const int PulseWire = 0;
 const int LED13 = 13;
 int Threshold = 550;
 int val;
+int myBPM;
 int tempPin = 1;
+int bmp = 0;
 unsigned long previousMillis = 0;
 const long interval = 10; // Réduit l'intervalle à 10 ms
 
 PulseSensorPlayground pulseSensor;
-SoftwareSerial blue(2, 3);
+SoftwareSerial blue(7, 8);
 
 void setup() {
   Serial.begin(9600);
-  blue.begin(9600);
+  blue.begin(1300);
   pulseSensor.analogInput(PulseWire);
   // pulseSensor.blinkOnPulse(LED13); // Désactivé pour tester
   pulseSensor.setThreshold(Threshold);
 
   if (pulseSensor.begin()) {
-    Serial.println("PulseSensor initialisé avec succès.");
+    //Serial.println("PulseSensor initialisé avec succès.");
   } else {
-    Serial.println("Erreur lors de l'initialisation du PulseSensor. Vérifiez le câblage et les paramètres.");
+    //Serial.println("Erreur lors de l'initialisation du PulseSensor. Vérifiez le câblage et les paramètres.");
   }
 }
 
 void loop() {
-  unsigned long currentMillis = millis();
-
-  if (currentMillis - previousMillis >= interval) {
-    previousMillis = currentMillis;
 
     // Read sensor values
-    int myBPM = pulseSensor.getBeatsPerMinute();
+    myBPM = analogRead(bmp);
     val = analogRead(tempPin);
     float mv = (val / 1024.0) * 5000;
     float cel = mv / 10;
@@ -46,5 +44,6 @@ void loop() {
 
     //blue.print("Temperature Sensor Value: ");
     blue.print(cell);
-  }
+    delay(0);
+  
 }
