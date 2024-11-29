46122c54e0e8f196f52cc7f74f3e46a69f6b0334
Fatima Zahra Mouket <fatimazahra30mouket@gmail.com>
2024-11-29 16:32:39 +0100

    Update CapteurRepository.java

--- src/main/java/com/appbiomedicale/repository/CapteurRepository.java
+++ src/main/java/com/appbiomedicale/repository/CapteurRepository.java
@@ -4,6 +4,7 @@ import com.appbiomedicale.domain.Capteur;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
 
+
 /**
  * Spring Data JPA repository for the Capteur entity.
  */
